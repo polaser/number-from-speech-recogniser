3f179d8932d70e38c6e9e185d3a60ecd330d8502
Pavel Proks <climbest@mail.ru>
2020-07-28 19:14:07 +0300

    Changed model and format of result

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Number from speech recogniser (Russian)
 
 1. Clone repository
-2. [Download model](https://drive.google.com/file/d/1HHh9Pcq-RbQUnRjHnjGdLsJZFYjvcRPt/view?usp=sharing) and place it near the script 'main.py'
+2. [Download model](https://drive.google.com/file/d/17U3D9RG7BLMwqK_EC89CyzouL7IbsxjE/view?usp=sharing) and place it near the script 'main.py'
 2. Install required libs
 3. Run main.py with one unnamed argument - path_to_your_file
 
--- main.py
+++ main.py
@@ -13,61 +13,14 @@ from scipy.io import wavfile
 from scipy import signal
 
 
-    
-dict_ = {
-    1: 'тринадцать',
-    2: 'два',
-    3: 'семнадцать',
-    4: 'пятнадцать',
-    5: 'девятнадцать',
-    6: 'пятьдесят',
-    7: 'девять',
-    8: 'десять',
-    9: 'двенадцать',
-    10: 'тысячи',
-    11: 'одиннадцать',
-    12: 'тысяч',
-    13: 'тридцать',
-    14: 'две',
-    15: 'четырнадцать',
-    16: 'семьдесят',
-    17: 'шестьсот',
-    18: 'шесть',
-    19: 'шестнадцать',
-    20: 'семьсот',
-    21: 'девятьсот',
-    22: 'семь',
-    23: 'пять',
-    24: 'сорок',
-    25: 'двести',
-    26: 'три',
-    27: 'пятьсот',
-    28: 'четыреста',
-    29: 'триста',
-    30: 'восемь',
-    31: 'сто',
-    32: 'шестьдесят',
-    33: 'одна',
-    34: 'тысяча',
-    35: 'четыре',
-    36: 'девяносто',
-    37: 'восемьсот',
-    38: 'восемьдесят',
-    39: 'один',
-    40: 'двадцать',
-    41: 'восемнадцать',
-    0: ' ',
-}
-
-
 if torch.cuda.is_available():
     device = 'cuda'
 else:
     device = 'cpu'
 
 model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet34', pretrained=False)
-model.fc = nn.Linear(in_features=512, out_features=42*8, bias=True)
-model.load_state_dict(torch.load('number_from_speech_resnet34_v0.pt'))
+model.fc = nn.Linear(in_features=512, out_features=10*6, bias=True)
+model.load_state_dict(torch.load('number_from_speech_resnet34_v1.pt'))
 model.to(device)
 
 
@@ -100,12 +53,12 @@ if len(sys.argv) > 1:
             input_ids = torch.tensor(input_ids).float().to(device)
 
             outputs = model(input_ids)
-            _preds = np.argmax(outputs.resize(_df.shape[0], 42, 8).cpu().numpy(), axis=1)
+            _preds = np.argmax(outputs.resize(_df.shape[0], 10, 6).cpu().numpy(), axis=1)
             for j in range(_df.shape[0]):
                 df.at[_df.index[j] ,'number'] = _preds[j]
                 
                 
-    df['number'] = df.number.apply(lambda x: ' '.join([dict_[i] for i in x]))
+    df['number'] = df.number.apply(lambda x: int(''.join(str(ii) for ii in x)))
 
     df[['path', 'number']].to_csv('result.csv', index=False)
 else:
